ecb889e5409ba3957caff87de0dbdbd0eee84692
hwoo3303 <hwoo3303@naver.com>
2016-06-28 12:28:40 +0900

    HR

---
+++ HR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HR
+//
+//  Created by Abraham Park on 6/21/16.
+//  Copyright © 2016 ebadaq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) IBOutlet UIWindow *window;
+
+
+@end
+
